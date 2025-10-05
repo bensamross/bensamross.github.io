fe73ca070b48c44b6385fe2016979e90b8c85263
Ben Ross <pingaross@gmail.com>
2025-10-05 11:18:48 +1000

    vite application with daisyUI installed

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './style.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <div className="card card-border bg-base-100 w-96">
+          <div className="card-body">
+            <h2 className="card-title">Card Title</h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Button</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
